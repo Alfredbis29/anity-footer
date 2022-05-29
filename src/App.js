4db6aead72f29cba6318369893c3626bc67e0249
Alfred <kalumunabisimwa5@gmail.com>
2022-05-29 16:39:13 +0200

    add footer

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="page-container">
+      <div className="content-wrap">
+       <h1>Anity footer</h1>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
