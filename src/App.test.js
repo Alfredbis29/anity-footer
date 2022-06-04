0061777795cc80ae15ca4f3bcafb339e5ee1c181
Alfred <kalumunabisimwa5@gmail.com>
2022-06-04 14:52:55 +0200

    add some features

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
+  // eslint-disable-next-line testing-library/prefer-screen-queries
   const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(linkElement).toBeInTheDocument()
 });
